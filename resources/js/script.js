ecaca3fa640c75147147d3c655f9331ba986c9ca
Oriol <orioldu@hotmail.com>
2026-04-29 02:12:35 +0200

    Updating my website

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,33 +1,33 @@
-const actualYear = new Date().getFullYear();
-const spanYear = document.getElementById("year");
-if (spanYear) {
-    spanYear.textContent = actualYear;
-}
-
-const updateProgress = () => {
-    const targetDate = new Date('2026-05-01T00:00:00').getTime();
-    const startDate = new Date('2026-04-01T00:00:00').getTime();
+const updateProjectProgress = (startDateStr, endDateStr, barId, textId) => {
+    const start = new Date(startDateStr).getTime();
+    const end = new Date(endDateStr).getTime();
     const now = new Date().getTime();
 
-    const total = targetDate - startDate;
-    const elapsed = now - startDate;
+    const total = end - start;
+    const elapsed = now - start;
 
     let percentage = (elapsed / total) * 100;
 
     percentage = Math.min(Math.max(percentage, 0), 100);
 
-    const bar = document.getElementById("progressBar");
-    const text = document.getElementById("progressText");
+    const bar = document.getElementById(barId);
+    const text = document.getElementById(textId);
 
     if (bar && text) {
-        bar.style.width = percentage + "%";
+        bar.style.width = percentage.toFixed(2) + "%";
         text.innerText = percentage.toFixed(2) + '% completado';
 
-    } else {
-        console.error("No se han encontrado los elementos 'progressBar' o 'progressText'");
+        if (now < start) text.innerText = "Próximamente...";
     }
-
 };
 
-setInterval(updateProgress, 1000);
-updateProgress();
+const runAllProgress = () => {
+    updateProjectProgress('2026-04-01T00:00:00', '2026-05-01T00:00:00', 'progressBar1', 'progressText1');
+
+    updateProjectProgress('2026-05-01T00:00:00', '2026-06-01T00:00:00', 'progressBar2', 'progressText2');
+
+    updateProjectProgress('2026-06-01T00:00:00', '2026-07-01T00:00:00', 'progressBar3', 'progressText3');
+};
+
+setInterval(runAllProgress, 1000);
+runAllProgress();
